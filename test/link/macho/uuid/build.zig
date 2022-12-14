b0bb6a2f6b1d9387e70632b6aac46bc5f9c6af02
Jakub Konka <kubkon@jakubkonka.com>
2022-12-14 23:57:35 +0100

    link-tests: strip in uuid test as it causes randomness (bug)

--- test/link/macho/uuid/build.zig
+++ test/link/macho/uuid/build.zig
@@ -7,32 +7,15 @@ pub fn build(b: *Builder) void {
     const test_step = b.step("test", "Test");
     test_step.dependOn(b.getInstallStep());
 
-    for (&[_]struct { mode: std.builtin.Mode, uuid: []const u8 }{
-        .{ .mode = .ReleaseSafe, .uuid = "6d0003ac18b137b99f315ff453e07d05" },
-        // .{ .mode = .ReleaseFast, .uuid = "30a45d10ba003a33bd57a4e98d3a6f6b" },
-        // .{ .mode = .ReleaseSmall, .uuid = "1361d54d7fb6300198375069749cc792" },
-    }) |exp| {
-        {
-            const dylib = simpleDylib(b, exp.mode);
-            const check_recipe = dylib.checkObject(.macho, .{});
-            const check = check_recipe.root();
-            check.match("cmd UUID");
-            check.match("uuid {x}");
-            const uuid = check.get("x");
-            uuid.eql(exp.uuid);
-            test_step.dependOn(&check_recipe.step);
-        }
-        {
-            // repeating the build should produce the same UUID
-            const dylib = simpleDylib(b, exp.mode);
-            const check_recipe = dylib.checkObject(.macho, .{});
-            const check = check_recipe.root();
-            check.match("cmd UUID");
-            check.match("uuid {x}");
-            const uuid = check.get("x");
-            uuid.eql(exp.uuid);
-            test_step.dependOn(&check_recipe.step);
-        }
+    for (&[_]std.builtin.Mode{ .ReleaseSafe, .ReleaseFast, .ReleaseSmall }) |mode| {
+        const dylib = simpleDylib(b, mode);
+        const check_recipe = dylib.checkObject(.macho, .{});
+        const check = check_recipe.root();
+        check.match("cmd UUID");
+        check.match("uuid {x}");
+        const uuid = check.get("x");
+        uuid.eql("1361d54d7fb6300198375069749cc792");
+        test_step.dependOn(&check_recipe.step);
     }
 }
 
@@ -42,5 +25,8 @@ fn simpleDylib(b: *Builder, mode: std.builtin.Mode) *LibExeObjectStep {
     dylib.setTarget(.{ .os_tag = .macos });
     dylib.addCSourceFile("test.c", &.{});
     dylib.linkLibC();
+    // TODO work on Zig-overwritten mtimes for debug info on macOS to always
+    // guarantee a fully reproducible build.
+    dylib.strip = true;
     return dylib;
 }
